ec0eb14c506b52b781ddf8e03b2e125430a2f8f6
Kora Hughes <korashughes@gmail.com>
2024-05-17 17:30:11 -0400

    Adding Additional Stats & Examples

---
+++ Data/example-code/python examples/programiz-python/basic.py
@@ -0,0 +1,14 @@
+
+
+num1 = input("first number:")
+num2 = input("second number:")
+
+def dist(num1, num2):
+    ...
+
+result = dist(num1, num2)
+print("The distance between these numbers is:")
+print(result)
+
+
+assert a == b
